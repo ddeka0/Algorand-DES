9c08367495721671fad48163e0a1142aa42631da
Abhishek Singh <you@example.com>
2019-04-14 17:21:15 +0530

    division

---
+++ client.c
@@ -0,0 +1,55 @@
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 12345
+
+
+
+
+void pfloat(char* s) 
+{
+
+    printf("float value : %4.8f\n" ,(float) atof(s)); 
+    return 0; 
+}
+
+int main(int argc, char const *argv[])
+{
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "divide:13432512353453451346346346342626345263463434:1000000000000000000000000000000000000000000000";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock, hello, strlen(hello), 0);
+    printf("divide sent\n");
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+    pfloat(buffer);
+    return 0;
+}
